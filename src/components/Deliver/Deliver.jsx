7d407465f253034767868c5779f9a8695cf0fe5e
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-11-05 23:36:48 +0500

    sa

---
+++ src/components/Deliver/Deliver.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Deliver.css";
+const Deliver = () => {
+  return (
+    <div className='deliver'>
+           <div className='container'>
+               <div className='deliver__container'>
+                     <ul className='deliver__list'>
+                        <li className='deliver__item'>
+                            
+                        </li>
+                     </ul>
+               </div>
+           </div>
+    </div>
+  )
+}
+
+export default Deliver
